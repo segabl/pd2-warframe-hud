9dfc92812e9c0fec6c57af3c575646ccbaa1d2ba
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-01-20 18:52:44 +0100

    Format bag carry text

---
+++ lua/hudtemp.lua
@@ -0,0 +1,9 @@
+Hooks:PostHook(HUDTemp, "init", "init_wfhud", function (self)
+	local bag_text = self._bg_box:child("bag_text")
+	bag_text:set_font(Idstring(tweak_data.menu.medium_font))
+	bag_text:set_font_size(24)
+	bag_text:set_align("right")
+	bag_text:set_x(0)
+end)
+
+function HUDTemp:set_throw_bag_text() end
