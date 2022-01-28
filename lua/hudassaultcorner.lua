640daa77587807a763322cf4f8dc40f31f00315e
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-01-28 13:33:21 +0100

    Enable and move assault panel to the left

--- lua/hudassaultcorner.lua
+++ lua/hudassaultcorner.lua
@@ -3,8 +3,16 @@ Hooks:PostHook(HUDAssaultCorner, "init", "init_wfhud", function (self)
 		return
 	end
 
-	self._hud_panel:child("assault_panel"):set_alpha(0)
-	self._hud_panel:child("assault_panel"):hide()
+	local h_offset = (WFHud:panel():w() - self._hud_panel:w()) * 0.5
+	local v_offset = (WFHud:panel():h() - self._hud_panel:h()) * 0.5
+
+	local assault_panel = self._hud_panel:child("assault_panel")
+	if assault_panel then
+		assault_panel:set_position(WFHud.MARGIN_H - h_offset, WFHud.MARGIN_V - v_offset)
+		assault_panel:child("icon_assaultbox"):set_left(self._bg_box_size + 3)
+		self._bg_box:set_position(0, 0)
+	end
+
 	self._hud_panel:child("hostages_panel"):set_alpha(0)
 	self._hud_panel:child("hostages_panel"):hide()
 	self._hud_panel:child("casing_panel"):set_alpha(0)
@@ -18,6 +26,11 @@ Hooks:PostHook(HUDAssaultCorner, "init", "init_wfhud", function (self)
 		self._hud_panel:child("wave_panel"):hide()
 	end
 end)
+
+local sync_set_assault_mode_original = HUDAssaultCorner.sync_set_assault_mode
+function HUDAssaultCorner:sync_set_assault_mode(mode, ...)
+	return sync_set_assault_mode_original(self, mode == "phalanx" and "normal" or mode, ...)
+end
 
 Hooks:OverrideFunction(HUDAssaultCorner, "show_point_of_no_return_timer", function (self, id)
 	local noreturn_data = self:_get_noreturn_data(id)
