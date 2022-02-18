2de2139339cc0d3047856e8450de03644fd4af05
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-02-19 00:14:46 +0100

    Add custom diesel icon

---
+++ lua/equipmentstweakdata.lua
@@ -0,0 +1,3 @@
+Hooks:PostHook(EquipmentsTweakData, "init", "init_wfhud", function (self)
+	self.specials.diesel.icon = "equipment_diesel"
+end)
---
+++ lua/tweakdatapd2.lua
@@ -0,0 +1,3 @@
+Hooks:PostHook(TweakData, "_init_pd2", "_init_pd2_wfhud", function (self)
+	self.hud_icons.equipment_diesel = self.hud_icons.equipment_gasoline
+end)
