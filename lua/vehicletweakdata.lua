41da578c892270d1c5f23268f5784b6fa51f0af8
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-02-21 23:08:55 +0100

    Increase Blackhawk label offset

--- lua/vehicletweakdata.lua
+++ lua/vehicletweakdata.lua
@@ -2,6 +2,7 @@
 Hooks:PostHook(VehicleTweakData, "init", "init_wfhud", function (self)
 	self.bike_1.hud_label_offset = 120
 	self.bike_2.hud_label_offset = 120
+	self.blackhawk_1.hud_label_offset = 300
 	self.boat_rib_1.hud_label_offset = 80
 	self.muscle.hud_label_offset = 140
 	self.muscle.hud_label_offset = 140
