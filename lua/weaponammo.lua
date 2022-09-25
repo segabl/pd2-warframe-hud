3ff45a436bf9c9b354cd98719855c1645be1e134
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-09-25 18:10:09 +0200

    Fix ammo pickup displaying incorrectly for weapons with underbarrels

---
+++ lua/weaponammo.lua
@@ -0,0 +1,31 @@
+local ids_texture = Idstring("texture")
+local ammo_icon_redirects = {
+	smg = "pistol",
+}
+
+local function set_ammo_total(self, ammo_total)
+	if self._setup and self._setup.user_unit ~= managers.player:player_unit() then
+		return
+	end
+
+	local current = self._ammo_total and self:digest_value(self._ammo_total, false) or math.huge
+	if current >= ammo_total then
+		return
+	end
+
+	local categories = self:weapon_tweak_data().categories
+	local category = categories[#categories]
+	local ammo_icon = "guis/textures/wfhud/hud_icons/pickup_ammo_" .. (ammo_icon_redirects[category] or category)
+	if not DB:has(ids_texture, Idstring(ammo_icon)) then
+		ammo_icon = "guis/textures/wfhud/hud_icons/pickup_ammo"
+	end
+	WFHud:add_pickup(category .. "_ammo", ammo_total - current, nil, ammo_icon)
+end
+
+if RaycastWeaponBase then
+	Hooks:PreHook(RaycastWeaponBase, "set_ammo_total", "set_ammo_total_wfhud", set_ammo_total)
+end
+
+if WeaponAmmo then
+	Hooks:PreHook(WeaponAmmo, "set_ammo_total", "set_ammo_total_wfhud", set_ammo_total)
+end
