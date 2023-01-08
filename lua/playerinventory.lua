3ff350619680c4670a5cc9cbe336bc19da5272f3
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-01-08 11:34:13 +0100

    Add sanity check to pECM buff display

--- lua/playerinventory.lua
+++ lua/playerinventory.lua
@@ -1,7 +1,11 @@
 Hooks:PostHook(PlayerInventory, "_start_jammer_effect", "_start_jammer_effect_wfhud", function (self)
-	WFHud:add_buff("player", "pocket_ecm_jammer_base", nil, self._jammer_data.t - TimerManager:game():time())
+	if self._jammer_data then
+		WFHud:add_buff("player", "pocket_ecm_jammer_base", nil, self._jammer_data.t - TimerManager:game():time())
+	end
 end)
 
 Hooks:PostHook(PlayerInventory, "_start_feedback_effect", "_start_feedback_effect_wfhud", function (self)
-	WFHud:add_buff("player", "pocket_ecm_jammer_base", nil, self._jammer_data.t - TimerManager:game():time())
+	if self._jammer_data then
+		WFHud:add_buff("player", "pocket_ecm_jammer_base", nil, self._jammer_data.t - TimerManager:game():time())
+	end
 end)
